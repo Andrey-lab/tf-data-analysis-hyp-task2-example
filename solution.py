e6e9760c87a107327000d146bcde3ac427ea6e4d
Andrew <62726008+Andrey-lab@users.noreply.github.com>
2023-04-13 23:57:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as st
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 297943594 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.06
+    a = st.anderson_ksamp([x, y]).significance_level
+    
+    return a < alpha 
